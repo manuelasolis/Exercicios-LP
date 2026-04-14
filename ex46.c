d83e8546bf3a1c761f9a61bafc182a263bc26614
manuelasolis <manuela.solis@unifesp.br>
2026-04-14 14:54:13 -0300

    Correct input/output and error handling in ex46.c
    Fix syntax errors in input and output functions.

---
+++ ex46.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    float v1, v2;
+
+    printf("v1: ");
+    scanf(%f,&v1)
+    prinft("v2: ");
+    scanf(%f,&v2);
+
+    while (v2==0);{
+        prinft("\nValor inválido");
+        printf("\nv1: ");
+        scanf("%f",&v2);
+    }
+    prinft ("%.1f / %1f = %1.f. \v",v1,v2,v1/v2);
+}
